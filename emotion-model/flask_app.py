9d664b00707b76d36efe2d6d707cce4a47cc851c
Manjiri Gole <119617326+manjirigole@users.noreply.github.com>
2025-04-28 23:39:48 +0530

    Update flask_app.py

--- emotion-model/flask_app.py
+++ emotion-model/flask_app.py
@@ -35,4 +35,4 @@ def predict():
     return jsonify({"sentiment": predicted_label, "confidence": confidence})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=False)
